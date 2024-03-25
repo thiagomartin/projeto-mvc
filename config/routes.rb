5293742fcd64bcbc3b728b683dd9782a27b15045
thiagomartin <155474906+thiagomartin@users.noreply.github.com>
2024-03-25 17:30:36 -0300

    add Resources

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
-  get 'products', to: "products#index"
-  get 'products/new', to: "products#new"
-  post 'products', to: "products#create"
-  get 'products/:id', to: "products#show"
+  ## root "posts#index"
+
+
+  resources :products
 
 end
